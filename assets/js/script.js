ed2ea5e02c68f7cd9379e6c090d4a9ebdc053391
jcha25 <justicech25@gmail.com>
2023-07-18 13:19:09 -0500

    set up js related questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,29 @@ var scores = 0;
 
 var quizValues = [
     {
-        question: "What color is the sky?",
-        choices: ["A. Green", "B. Brown", "C. Blue", "D. Red"],
-        correct: "C. Blue"
+        question: "What attribute goes in an href to send someone to another webiste?",
+        choices: ["A. src=''", "B. href=''", "C. link=''", "D. charset=''"],
+        correct: "B. href=''"
+    },
+    {
+        question: "What is a CSS file for?",
+        choices: ["A. functionality", "B. skelaton of page", "C. styling", "D. Math"],
+        correct: "C. Styling"
+    },
+    {
+        question: "which tag do we use to link a js file to html?",
+        choices: ["A. Link", "B. Anchor", "C. Script", "D. Meta"],
+        correct: "C. Script"
+    },
+    {
+        question: "What is a h1-5 tag for?",
+        choices: ["A. Paragraphs", "B. Lists", "C. Navigation", "D. Headers"],
+        correct: "D. Headers"
+    },
+    {
+        question: "What tag is used for ordered lists?",
+        choices: ["A. ul", "B. li", "C. a", "D. ol"],
+        correct: "D. ol"
     }
 ]
 
